f65992066d89ce726f46dc26d53f94c570ab7fcd
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2024-05-12 13:43:16 +0700

    Complete the file 0-1-String-Primitive_Type.js

---
+++ 3. Data Types [BASIC]/0-1-String-Primitive_Type.js
@@ -0,0 +1,8 @@
+// CHUỖI (STRING)
+/*
+* Trong JS, chuỗi String là 1 chuỗi các ký tự Unicode, được bao quanh bởi dấu ngoặc đơn ('') hoặc dấu ngoặc kép ("").
+*/
+
+// Code cơ bản:
+let myString = "Hoang Kien";
+console.log(myString);
