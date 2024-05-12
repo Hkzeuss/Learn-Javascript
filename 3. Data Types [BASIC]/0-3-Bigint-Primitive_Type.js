6bb7fd1380e836faedf4ba41fcf31d82574ce692
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2024-05-12 13:51:31 +0700

    Complete the file 0-3-Bigint-Primitive_Type.js

---
+++ 3. Data Types [BASIC]/0-3-Bigint-Primitive_Type.js
@@ -0,0 +1,9 @@
+// SỐ NGUYÊN LỚN (Bigint)
+
+/*
+* - Bigint là kiểu dữ liệu mới trong JS để đại diện cho các số nguyên lớn hơn 2^53
+*/
+
+// Code cơ bản:
+const bigintNumber = 76346784367834678345;
+console.log(bigintNumber);
