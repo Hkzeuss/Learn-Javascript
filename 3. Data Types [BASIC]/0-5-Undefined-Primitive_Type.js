7359ae205fb7c252767c8f9d4de3b39f70ac2ddd
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2024-05-12 13:56:39 +0700

    Complete the file 0-5-Undefined-Primitive_Type.js

---
+++ 3. Data Types [BASIC]/0-5-Undefined-Primitive_Type.js
@@ -0,0 +1,9 @@
+// CHƯA XÁC ĐỊNH (UNDEFINED)
+
+/*
+* - Là giá trị của 1 biến chưa được gán hoặc 1 biến không có giá trị trả về
+*/
+
+// Code cơ bản:
+let undeFinedVar;
+console.log(undeFinedVar); // Output: undefined
