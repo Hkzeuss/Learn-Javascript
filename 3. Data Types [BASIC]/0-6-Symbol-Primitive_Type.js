05a9165adc7d249306d2f0d9552bbf39ebca7b48
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2024-05-12 13:59:29 +0700

    Complete the file 0-6-Symbol-Primitive_Type.js

---
+++ 3. Data Types [BASIC]/0-6-Symbol-Primitive_Type.js
@@ -0,0 +1,9 @@
+// KÝ HIỆU (SYMBOL)
+
+/*
+* - Là kiểu dữ liệu độc nhất, không thể so sánh với bất kỳ symbol nào khác 
+*/
+
+// Code cơ bản:
+const mySymbol = Symbol('Hoang Kien');
+console.log(mySymbol);
