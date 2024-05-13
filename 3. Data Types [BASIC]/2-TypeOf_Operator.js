9309e59307f5c43f9bc42f3a12b7cd05a1ce8e5e
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2024-05-13 16:13:23 +0700

    Complete the file 2-TypeOf_Operator.js

---
+++ 3. Data Types [BASIC]/2-TypeOf_Operator.js
@@ -0,0 +1,22 @@
+// TOÁN TỬ TypeOf
+
+// Code cơ bản:
+// Khai báo các biến với các giá trị khác nhau để thử nghiệm toán tử typeOf
+let num = 0;
+let str = "Hoang Kien";
+let bool = false;
+let arr = [1, 2, 3];
+let obj = {name:"Kien", age: 20};
+let func = function () {return 'Function'};
+let nu = null;
+let und = undefined;
+
+// In ra kiểu dữ liệu của từng biến:
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof arr);
+console.log(typeof obj);
+console.log(typeof func);
+console.log(typeof nu);
+console.log(typeof und);
